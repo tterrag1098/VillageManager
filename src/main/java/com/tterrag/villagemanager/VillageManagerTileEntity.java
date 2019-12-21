e8a2349cff453b2e0cd98ebd4066b8eeb26f34a6
tterrag <tterrag1098@gmail.com>
2019-12-21 01:55:54 -0500

    Some more experiments

--- src/main/java/com/tterrag/villagemanager/VillageManagerTileEntity.java
+++ src/main/java/com/tterrag/villagemanager/VillageManagerTileEntity.java
@@ -1,21 +1,26 @@
 package com.tterrag.villagemanager;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.google.common.base.Predicates;
+import com.google.common.collect.Sets;
 
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.village.PointOfInterest;
 import net.minecraft.village.PointOfInterestManager;
 import net.minecraft.village.PointOfInterestManager.Status;
-import net.minecraft.world.ServerWorld;
+import net.minecraft.world.server.ServerWorld;
 
-public class VillageManagerTileEntity extends TileEntity {
+public class VillageManagerTileEntity extends TileEntity implements ITickableTileEntity {
 	
-	public static final Set<PointOfInterest> POIS = new HashSet<>();
+	public static final Set<PointOfInterest> POIS = Sets.newConcurrentHashSet();
+	public static final Set<VillagerEntity> VILLAGERS = Sets.newConcurrentHashSet();
 
 	public VillageManagerTileEntity() {
 		super(VillageManager.TILE_TYPE);
@@ -27,8 +32,21 @@ public class VillageManagerTileEntity extends TileEntity {
 		if (getWorld().isRemote) return;
 		POIS.clear();
 		
-		PointOfInterestManager pois = ((ServerWorld)getWorld()).func_217443_B();
+		PointOfInterestManager pois = ((ServerWorld)getWorld()).getPointOfInterestManager();
 		POIS.addAll(pois.func_219146_b(Predicates.alwaysTrue(), getPos(), 256, Status.ANY).collect(Collectors.toSet()));
+	}
+	
+	@Override
+	public void tick() {
+	    if (getWorld().isRemote) return;
+	    if (world.getGameTime() % 100 == 0) {
+	        updateNearby();
+	    }
+	}
+	
+	private void updateNearby() {
+	    VILLAGERS.clear();
+	    VILLAGERS.addAll(getWorld().getEntitiesWithinAABB(VillagerEntity.class, new AxisAlignedBB(getPos()).grow(256)));
 	}
 	
 	@Override
--- src/main/java/com/tterrag/villagemanager/VillageManagerTileEntityRenderer.java
+++ src/main/java/com/tterrag/villagemanager/VillageManagerTileEntityRenderer.java
@@ -1,5 +1,9 @@
 package com.tterrag.villagemanager;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.lwjgl.opengl.GL11;
 
 import com.mojang.blaze3d.platform.GLX;
@@ -10,7 +14,11 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.Vector4f;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.entity.ai.brain.memory.MemoryModuleType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.GlobalPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.village.PointOfInterest;
 import net.minecraft.village.PointOfInterestType;
 
@@ -19,32 +27,77 @@ public class VillageManagerTileEntityRenderer extends TileEntityRenderer<Village
 	@Override
 	public void render(VillageManagerTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage) {
 		BufferBuilder buf = Tessellator.getInstance().getBuffer();
-		buf.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
-		buf.setTranslation(x, y, z);
-		
+
 		GlStateManager.disableTexture();
 		GlStateManager.lineWidth(4);
 		GlStateManager.disableLighting();
+		GlStateManager.enableBlend();
         GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, 240F, 240F);
 		
-		for (PointOfInterest poi : VillageManagerTileEntity.POIS) {
-			BlockPos pos = poi.getPos().subtract(tileEntityIn.getPos());
-			Vector4f color = new Vector4f(1, 1, 1, 1);
-			if (poi.isOccupied()) {
-				color.set(0, 1, 0, 1);
-			} else {
-				color.set(1, 1, 0, 1);
-			}
-			if (poi.getType() == PointOfInterestType.HOME) {
-				color.set(0, 0, 1, 1);
-			}
-			buf.pos(0.5, 0.5, 0.5).color(color.getX(), color.getY(), color.getZ(), color.getW()).endVertex();
-			buf.pos(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5).color(color.getX(), color.getY(), color.getZ(), color.getW()).endVertex();
-		}
-		Tessellator.getInstance().draw();
+        for (int i = 0; i < 2; i++) {
+            buf.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
+            buf.setTranslation(x, y, z);
+            
+            if (i == 0) {
+                GlStateManager.disableDepthTest();
+            } else {
+                GlStateManager.enableDepthTest();
+            }
+    		for (PointOfInterest poi : VillageManagerTileEntity.POIS) {
+    			BlockPos pos = poi.getPos();
+    			if (pos.distanceSq(tileEntityIn.getPos()) > 150 * 150) continue;
+    			Vector4f color = new Vector4f(1, 1, 1, 1);
+    			List<Vec3d> starts = new ArrayList<>();
+    			if (poi.isOccupied()) {
+    				color.set(0, 1, 0, 1);
+    			} else {
+                    color.set(1, 0.5f, 0, 1);
+    			}
+    			if (poi.getType() == PointOfInterestType.HOME) {
+    			    starts = getStarts(pos, tileEntityIn.getPos(), partialTicks, MemoryModuleType.HOME);
+    			    if (poi.isOccupied()) {
+    			        color.set(0, 0, 1, 1);
+    			    } else {
+                        color.set(1, 0, 0, 1);
+    			    }
+    			} else {
+                    starts = getStarts(pos, tileEntityIn.getPos(), partialTicks, MemoryModuleType.JOB_SITE);
+                    if (starts.isEmpty()) {
+                        starts = getStarts(pos, tileEntityIn.getPos(), partialTicks, MemoryModuleType.MEETING_POINT);
+                        if (!starts.isEmpty()) {
+                            color.set(1, 1, 0, 1);
+                        }
+                    }
+    			}
+    			if (starts.isEmpty()) {
+                    starts.add(new Vec3d(0.5, 0.5, 0.5));
+    			}
+    			if (i == 0) {
+    			    color.set(color.getX(), color.getY(), color.getZ(), color.getW() * 0.25f);;
+    			}
+                BlockPos renderPos = pos.subtract(tileEntityIn.getPos());
+    			for (Vec3d start : starts) {
+        			buf.pos(start.x, start.y, start.z).color(color.getX(), color.getY(), color.getZ(), color.getW()).endVertex();
+        			buf.pos(renderPos.getX() + 0.5, renderPos.getY() + 0.5, renderPos.getZ() + 0.5).color(color.getX(), color.getY(), color.getZ(), color.getW()).endVertex();
+    			}
+    		}
+    		Tessellator.getInstance().draw();
+        }
+
 		buf.setTranslation(0, 0, 0);
 		
 		GlStateManager.enableTexture();
+		GlStateManager.enableDepthTest();
+	}
+	
+	private List<Vec3d> getStarts(BlockPos poiPos, BlockPos tilePos, float partialTicks, MemoryModuleType<?> type) {
+	    return VillageManagerTileEntity.VILLAGERS.stream().filter(v -> v.getBrain().getMemory(type).map(gp -> 
+                gp.equals(GlobalPos.of(getWorld().getDimension().getType(), poiPos))).orElse(false))
+                    .map(v -> 
+                        new Vec3d(MathHelper.lerp(partialTicks, v.lastTickPosX, v.posX), MathHelper.lerp(partialTicks, v.lastTickPosY, v.posY), MathHelper.lerp(partialTicks, v.lastTickPosZ, v.posZ))
+                        .add(0, v.getHeight() / 2, 0)
+                        .subtract(new Vec3d(tilePos)))
+                    .collect(Collectors.toList());
 	}
 
 	@Override
